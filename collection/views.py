5dcaade3ecd8e1e0ea479af98ab7a83ba4397a7f
Richard S Hill <hello@richardshill.co.uk>
2017-02-15 20:51:55 +0000

    Commit before moving on to dynamic data
    Ok, everything’s working at this point…

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request): 
-    # this is your new view
-    return render(request, 'index.html')
+
+def index(request):
+    # defining the variable
+    number = 6
+    # passing the variable to the view
+    return render(request, 'index.html', {
+        'number': number,
+    })
