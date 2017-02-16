a30a93944353ca7308296ed4206e56207739192e
Richard S Hill <hello@richardshill.co.uk>
2017-02-16 21:12:19 +0000

    Setting up the database
    Creating superuser for admin and the Audit name for the collection of
    things I’m going to build!

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from collection.models import Audit
+
+# set up automated slug creation
+class AuditAdmin(admin.ModelAdmin):
+    model = Audit
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Audit, AuditAdmin)
+
--- collection/models.py
+++ collection/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-
-
+class Audit(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
